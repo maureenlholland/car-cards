53adf377d591498a4d22c4ac78a3a8f9c88353a3
maureenlholland <maureen.holland11@gmail.com>
2020-05-22 20:23:28 -0400

    Add hooks to control text inputs

--- src/components/CarForm.jsx
+++ src/components/CarForm.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
   Pane,
   IconButton,
@@ -13,22 +13,49 @@ import {
 
 
 function CarForm() {
+  const [make, setMake] = useState('');
+  const [model, setModel] = useState('');
+  const [link, setLink] = useState('');
+  const [highPrice, setHighPrice] = useState('');
+  const [lowPrice, setLowPrice] = useState('');
+  const [height, setHeight] = useState('');
+  const [width, setWidth] = useState('');
+  const [length, setLength] = useState('');
+  const [attribute, setAttribute] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    // clean data
+    // call firebase to save car
+      // on success, redirect to new car card 
+      // on failure, display errors
+  }
+
+  const addAttribute = (e) => {
+    e.preventDefault();
+    // add checkbox with new attribute, default checked
+  }
+
   return (
     <Pane className="car__form">
       {/* pre-fill form if editing existing car, show blank if new */}
       <Heading is="h2">Car Form</Heading>
-      <form>
+      <form onSubmit={(e) => handleSubmit(e)}>
         <TextInputField
           label="Make"
           name="make"
           placeholder="Mazda"
           required
+          value={make}
+          onChange={(e) => setMake(e.target.value)}
         />
         <TextInputField
           label="Model"
           name="Model"
           placeholder="CX-5"
           required
+          value={model}
+          onChange={(e) => setModel(e.target.value)}
         />
         {/* on change, show selected field */}
         <SelectField
@@ -49,6 +76,8 @@ function CarForm() {
           name="link"
           type="url"
           placeholder="https://www.mazda.ca/en/vehicles/cx-5/overview/"
+          value={link}
+          onChange={(e) => setLink(e.target.value)}
         />
         <RadioGroup
           label="Status"
@@ -61,13 +90,17 @@ function CarForm() {
         >
           <TextInputField
             label="High"
-            name="high"
+            name="highPrice"
             placeholder="$43,965"
+            value={highPrice}
+            onChange={(e) => setHighPrice(e.target.value)}
           />
           <TextInputField
             label="Low"
-            name="low"
+            name="lowPrice"
             placeholder="$30,015"
+            value={lowPrice}
+            onChange={(e) => setLowPrice(e.target.value)}
           />
         </FormField>
         <FormField
@@ -77,16 +110,22 @@ function CarForm() {
             label="Length"
             name="length"
             placeholder="4550mm"
+            value={length}
+            onChange={(e) => setLength(e.target.value)}
           />
           <TextInputField
             label="Width"
             name="width"
             placeholder="1840mm"
+            value={width}
+            onChange={(e) => setWidth(e.target.value)}
           />
           <TextInputField
             label="Height"
             name="height"
             placeholder="1675mm"
+            value={height}
+            onChange={(e) => setHeight(e.target.value)}
           />
         </FormField>
         <FormField
@@ -103,13 +142,15 @@ function CarForm() {
           label="New Attribute"
           name="attribute"
           placeholder="Something I want"
+          value={attribute}
+          onChange={(e) => setAttribute(e.target.value)}
         />
-        <Button appearance="primary">Add</Button>
+        <Button appearance="primary" type="button" onClick={(e) => addAttribute(e)}>Add</Button>
         {/* on click, transform to save button, show and focus input for new attribute */}
         <IconButton icon="plus" />
         {/* on click, save to db, redirect to new single car page */}
         {/* if error, display error message */}
-        <Button appearance="primary">Save Car</Button>
+        <Button appearance="primary" type="submit">Save Car</Button>
       </form>
     </Pane>
   );
