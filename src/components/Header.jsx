d17ef595a4679d8675d5ec7fc1022a49e99c8ea6
maureenlholland <maureen.holland11@gmail.com>
2020-05-24 12:56:39 -0400

    Add home content, update styles

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,19 +1,20 @@
 import React from 'react';
+import { useRouteMatch } from "react-router-dom";
 import { Link as NavLink } from "react-router-dom";
 import { Heading, Button } from 'evergreen-ui';
 
 export default function Header() {
+    const isHome = useRouteMatch('/').isExact;
+
     return (
         <header>
             <div className="wrapper">
-                {/* add home button icon before text */}
-                <Heading is="h1" size={900} color="white" paddingBottom="10px">Car Cards</Heading>
-                {/* if home, no button */}
-                {/* if deck, label of deck and add new card button */}
-                {/* if car or form, go back button */}
-                <Button height="34">
-                    <NavLink style={{ textDecoration: 'none', color: 'inherit' }} to="/car/form">Add new card</NavLink>
-                </Button>
+                <Heading is="h1" size={900} color="white">Car Cards</Heading>
+                {!isHome && (
+                    <Button height="34" marginTop="10px">
+                        <NavLink style={{ textDecoration: 'none', color: 'inherit' }} to="/car/form">Add new card</NavLink>
+                    </Button>
+                )}
             </div>
         </header>
     )
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,11 +1,43 @@
-// Home page
-// Redirect to login if user not identified
-// Show add new deck button if user has no decks (like attribute, just a label field)
-// Redirect to only deck if user has 1 deck
-// Show list of decks if user has more than 1 deck
-
 import React from 'react';
+import { Link as NavLink } from "react-router-dom";
+import {
+  Card,
+  Pane,
+  Heading,
+  UnorderedList,
+  ListItem,
+  Button,
+  Paragraph,
+} from 'evergreen-ui';
+import decks from '../mockData/decks';
+
+function DeckCard({ deck }) {
+    return (
+      <ListItem className="car-cards__single">
+        <Card elevation={2} padding="20px" background="white">
+          <Heading is="h2">{deck.label}</Heading>
+          <NavLink style={{ textDecoration: 'none', color: 'inherit' }} to={`/${deck.id}`}>
+            <Button marginTop="20px"  iconAfter="arrow-right">See deck</Button>
+          </NavLink>
+        </Card>
+      </ListItem>
+    );
+  }
 
 export default function Home() {
-    return <div>Home stub</div>
+    // Redirect to login if user not identified
+    // Show add new deck button if user has no decks (like attribute, just a label field)
+    // Show list of decks if user has decks
+
+    // on mount, api call to get decks by user
+    const deck = decks[0];
+    return (
+        <Pane className={`deck deck--${deck.id}`}>
+            {decks.length === 0 ? <Paragraph>No decks yet!</Paragraph> : (
+                <UnorderedList>
+                    {decks.map((d) => <DeckCard key={d.id} deck={d}/>)}
+                </UnorderedList>
+            )}
+        </Pane>
+    );
 }
